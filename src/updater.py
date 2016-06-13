e78429d6fcce337068c55ed9f8524002c897d280
Konstantinos Paparas <kelsos86@gmail.com>
2016-06-14 00:04:52 +0300

    Changes the window type

--- src/updater.py
+++ src/updater.py
@@ -6,7 +6,7 @@ from gi.repository import Gtk
 
 class Updater:
 
-  def on_window1_destroy(self, object, data=None):
+  def on_app_window_destroy(self, object, data=None):
     print("quit with cancel")
     Gtk.main_quit()
 
@@ -15,32 +15,14 @@ class Updater:
     Gtk.main_quit()
 
   def __init__(self):
-    self.gladefile = "main_ui.glade"
+    self.gladefile = 'main_ui.glade'
     self.builder = Gtk.Builder()
-    self.builder.add_from_file(self.gladefile)
+    self.builder.add_objects_from_file(self.gladefile, ('app_window', ''))
     self.builder.connect_signals(self)
-    self.window = self.builder.get_object("app_window")
-    self.window.show()
+    self.window = self.builder.get_object('app_window')
+    self.window.show_all()
 
 if __name__ == "__main__":
   main = Updater()
   Gtk.main()
-        # datastore = DataStore()
-        # utilities.load_installed_versions(datastore)
-        # utilities.check_for_updates(datastore)
-        #
-        # idea_installed_version.set_text(datastore.idea.installed_version)
-        # php_installed_version.set_text(datastore.phpstorm.installed_version)
-        # py_installed_version.set_text(datastore.pycharm.installed_version)
-        # rb_installed_version.set_text(datastore.rubymine.installed_version)
-        # web_installed_version.set_text(datastore.webstorm.installed_version)
-        # c_installed_version.set_text(datastore.clion.installed_version)
-        # db_installed_version.set_text(datastore.dbe.installed_version)
-        #
-        # idea_available_version.set_text(datastore.idea.available_version)
-        # php_available_version.set_text(datastore.phpstorm.available_version)
-        # py_available_version.set_text(datastore.pycharm.available_version)
-        # rb_available_version.set_text(datastore.rubymine.available_version)
-        # web_available_version.set_text(datastore.webstorm.available_version)
-        # c_available_version.set_text(datastore.clion.available_version)
-        # db_available_version.set_text(datastore.dbe.available_version)
+
